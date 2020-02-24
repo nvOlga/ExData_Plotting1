25d9754dce62edbff3d3ba73de486affa829b5aa
Olga <olga.nev@gmail.com>
2020-02-24 20:49:43 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,23 @@
+library("data.table")
+setwd("/Users/useroid/Dropbox/Learnings/R/Exploratory Data Analyses/data")
+
+#Reads in data from file then subsets data for specified dates
+power <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+
+# Prevents Scientific Notation
+power[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+# Making a POSIXct date capable of being filtered and graphed by time of day
+power[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+power <- power[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+## Plot 2
+plot(x = power[, dateTime]
+     , y = power[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
